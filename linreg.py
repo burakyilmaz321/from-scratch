764f379d65b2ce76bd96f1dacf0f93f4d917162b
Burak Yılmaz <burakyilmaz321@gmail.com>
2017-07-23 15:45:21 +0300

    add linear regression with neural network

---
+++ linreg.py
@@ -0,0 +1,31 @@
+'''
+Linear regression with a single percceptron network
+'''
+import numpy as np
+
+def sigm(x):
+    return 1 / (1 + np.exp(-x))
+
+def feedforward(x, w, y):
+    a = sigm(x * w)
+    e = (y - a)**2 / 2
+    return (a, e)
+    
+def backprop(a, w, y):
+    delta = (a - y) * a * (1 - a)
+    w_updated = w - delta
+    return w_updated
+
+def train(x, w, y, epoch):
+    for _ in range(epoch):
+        a, e = feedforward(x, w, y)
+        if _ % (epoch / 10) == 0:
+            print('epoch ' + str(_))
+            print('    Error: {:.4f}'.format(e))
+            print('    Output: {:.4f}'.format(a))
+            print('')
+        w = backprop(a, w, y)
+    
+    print('Final error: {:.4f}'.format(e))
+    print('Final weight: {:.4f}'.format(w))
+    print('Final output: {:.4f}'.format(a))
