58bbd2823a25527c48faa536c177238e2ba767de
Burak Yılmaz <burakyilmaz321@gmail.com>
2017-08-02 18:39:00 +0300

    add number of units parameters. closed issue #1

--- neural_network.R
+++ neural_network.R
@@ -14,14 +14,19 @@ iris[, x4 := (iris$Petal.Width - min(iris$Petal.Width)) / (max(iris$Petal.Width)
 iris[Species == 'setosa', y := 1]
 iris[Species == 'versicolor', y := 0]
 
-# Initialize Network
-n <- nrow(iris)
-
+# Prepare data
 a1 <- as.matrix(cbind(rep(1, n), iris[, 7:10]))
 y <- as.matrix(iris[, y])
 
-w1 <- matrix(rnorm(20), 5, 4)
-w2 <- matrix(rnorm(4))
+# Parameters
+HIDDEN_UNITS <- 4
+INPUT_UNITS <- dim(a1)[2]
+OUTPUT_UNITS <- dim(y)[2]
+N <- nrow(a1)
+
+# Initialize Network
+w1 <- matrix(rnorm(INPUT_UNITS * HIDDEN_UNITS), INPUT_UNITS, HIDDEN_UNITS)
+w2 <- matrix(rnorm(HIDDEN_UNITS * OUTPUT_UNITS), HIDDEN_UNITS, OUTPUT_UNITS)
 
 sigm <- function(x) { 1 / (1 + exp(-x)) }
 
@@ -33,6 +38,6 @@ for (i in 1:100000) {
   d1 <- d2 %*% t(w2) * a2 * (1 - a2);
   w2 <- w2 - 0.5 * (t(a2) %*% d2);
   w1 <- w1 - 0.5 * (t(a1) %*% d1); 
-  e <- (y - a3)**2 / (2 * n)
+  e <- (y - a3)**2 / (2 * N)
 }
 
