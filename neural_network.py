e552c1b893d240f9c1c37bd0280f2f2c55aea75b
Burak Yılmaz <burakyilmaz321@gmail.com>
2017-08-13 20:27:00 +0300

    added leaky relu

--- neural_network.py
+++ neural_network.py
@@ -1,19 +1,31 @@
 '''
-Basic 3-layer neural network implementation. 
+Basic 3-layer neural network implementation.
 '''
 
 import numpy as np
 
 class ActivationFunctions(object):
+    '''
+    Class for activation unit functions and their first order conditions
+    Avaialable functions for now:
+        (1) Sigmoid
+        (2) Rectified Linear Units (ReLu)
+        (3) Leaky Rectified Linear Units (Leaky ReLu)
+    Tanh and softmax might be added in the future
+    '''
+    # Sigmoid
     def sigm(x):
         return 1 / (1 + np.exp(-x))
 
+    # Derivative of Sigmoid
     def sigm_d(x):
         return x * (1 - x)
 
+    # Rectified Linear Units
     def relu(x):
         return np.maximum(0, x)
-    
+
+    # Derivative of Rectified Linear Units
     def relu_d(x):
         def f(x):
             if x > 0:
@@ -22,25 +34,40 @@ class ActivationFunctions(object):
                 return 0
         return np.vectorize(f)(x)
 
+    # Leaky Rectified Linear Units
+    def lrelu(x):
+        return np.maximum(0, 0.01 * x)
+
+    # Derivative of Leaky Rectified Linear Units
+    def lrelu_d(x):
+        def f(x):
+            if x > 0:
+                return 0.01
+            else:
+                return 0
+        return np.vectorize(f)(x)
+
+
 class NeuralNetwork(object):
     '''
-    Description: 3-Layer perceptron class. 
+    Description: 3-Layer perceptron class.
     Topology: [#of input units, #of hidden units, #of output units]
     Activation: 'sigm' or 'relu'
     Alpha: learning rate between 0 and 1
     '''
     activations = {
         'sigm': (ActivationFunctions.sigm, ActivationFunctions.sigm_d),
-        'relu': (ActivationFunctions.relu, ActivationFunctions.relu_d)
+        'relu': (ActivationFunctions.relu, ActivationFunctions.relu_d),
+        'lrelu': (ActivationFunctions.lrelu, ActivationFunctions.lrelu_d)
     }
 
     errors = []
 
     predictions = None
-    
+
     def __init__(self, topology, activation, alpha):
         self.topology = topology
-        self.activation = activation    
+        self.activation = activation
         self.alpha = alpha
 
     def report(self, x, y, e, epoch, iter):
@@ -56,7 +83,7 @@ class NeuralNetwork(object):
         a4 = self.activations['sigm'][0](np.dot(a3, w3))
         e = (y - a4)**2 / 2
         return (a2, a3, a4, e)
-        
+
     def backprop(self, a1, a2, a3, a4, w1, w2, w3, y):
         delta3 = (a4 - y) * self.activations['sigm'][1](a4)
         delta2 = np.dot(delta3, w3.T) * self.activations[self.activation][1](a3)
@@ -75,7 +102,7 @@ class NeuralNetwork(object):
     def fit(self, X, y, epoch, silent=False):
         '''
         X, y: numpy 1-D array. Will be reshaped according to the network topology
-        '''        
+        '''
         n = len(X) // self.topology[0]
 
         # Input space
@@ -95,7 +122,7 @@ class NeuralNetwork(object):
             a2, a3, a4, e = self.feedforward(a1, w1, w2, w3, y)
             w1, w2, w3 = self.backprop(a1, a2, a3, a4, w1, w2, w3, y)
             self.errors.append(np.mean(e))
-            if not silent: 
+            if not silent:
                 self.report(a4, y, e, epoch, _)
 
         print('Final error : {:.4f}'.format(np.mean(e)))
@@ -109,8 +136,8 @@ def main():
     '''
     Main function initializes the input space, the output space and the network
     Input and output should be numpy arrays.
-    Network instance must be initialized with topology, learning rate, activation function 
-    To train the network, input and output arrays must be passed as an argument 
+    Network instance must be initialized with topology, learning rate, activation function
+    To train the network, input and output arrays must be passed as an argument
     as well as the desired number of training epochs
     '''
     X = np.array([0,0,1,1,0,1,0,1])
